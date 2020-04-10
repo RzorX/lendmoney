907fa8c08d9554cdb8fef0673546df29e986bb57
RzorX <49616719+RzorX@users.noreply.github.com>
2020-04-10 11:38:56 -0300

    add main.js

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,14 @@
 let choose = document.getElementById("emprestimo");
+
+function calcparcelas(){
+    var a = "6x";
+    var b = "8x";
+    var c = "10x";
+    var d = "12x";
+    var e = "18x";
+    document.getElementById("10k").innerHTML = a; 
+    document.getElementById("20k").innerHTML = b; 
+    document.getElementById("30k").innerHTML = c; 
+    document.getElementById("40k").innerHTML = d; 
+    document.getElementById("50k").innerHTML = e;
+}
