cc9e5f969f02468fea555d2c9a006f8d2693caf7
RzorX <49616719+RzorX@users.noreply.github.com>
2020-04-10 12:15:03 -0300

    update main.js

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,14 +1,21 @@
 let choose = document.getElementById("emprestimo");
 
-function calcparcelas(){
+function calcparcelas() {
     var a = "6x";
     var b = "8x";
     var c = "10x";
     var d = "12x";
     var e = "18x";
-    document.getElementById("10k").innerHTML = a; 
-    document.getElementById("20k").innerHTML = b; 
-    document.getElementById("30k").innerHTML = c; 
-    document.getElementById("40k").innerHTML = d; 
+    var f = "Voltar";
+    document.getElementById("10k").innerHTML = a;
+    document.getElementById("20k").innerHTML = b;
+    document.getElementById("30k").innerHTML = c;
+    document.getElementById("40k").innerHTML = d;
     document.getElementById("50k").innerHTML = e;
+    document.getElementById("value_alt").innerHTML = f;
+    document.getElementById("parc").innerHTML = "Em quantas vezes?";
+}
+
+function digitarvalor() {
+    var valor;
 }
