03381d3e2ffa91f0fd245d9a2523667473915097
RzorX <49616719+RzorX@users.noreply.github.com>
2020-04-11 18:49:21 -0300

    add maps

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,33 +1,34 @@
-let choose = document.getElementById("emprestimo");
+/* global google */
 
-function calcparcelas() {
-    var a = "6x";
-    var b = "8x";
-    var c = "10x";
-    var d = "12x";
-    var e = "18x";
-    var f = "Voltar";
-    document.getElementById("10k").innerHTML = a;
-    document.getElementById("20k").innerHTML = b;
-    document.getElementById("30k").innerHTML = c;
-    document.getElementById("40k").innerHTML = d;
-    document.getElementById("50k").innerHTML = e;
-    document.getElementById("value_alt").innerHTML = f;
-    document.getElementById("parc").innerHTML = "Em quantas vezes?";
-}
+let choose = document.getElementById("emprestimo");
+        function calcparcelas() {
+        var a = "6x";
+                var b = "8x";
+                var c = "10x";
+                var d = "12x";
+                var e = "18x";
+                var f = "Voltar";
+                document.getElementById("10k").innerHTML = a;
+                document.getElementById("20k").innerHTML = b;
+                document.getElementById("30k").innerHTML = c;
+                document.getElementById("40k").innerHTML = d;
+                document.getElementById("50k").innerHTML = e;
+                document.getElementById("value_alt").innerHTML = f;
+                document.getElementById("parc").innerHTML = "Em quantas vezes?";
+                }
 
 function digitarvalor() {
-    var a = "R$ 10.000";
-    var b = "R$ 20.000";
-    var c = "R$ 30.000";
-    var d = "R$ 40.000";
-    var e = "R$ 50.000";
-    var f = "Outro Valor";
-    document.getElementById("10k").innerHTML = a;
-    document.getElementById("20k").innerHTML = b;
-    document.getElementById("30k").innerHTML = c;
-    document.getElementById("40k").innerHTML = d;
-    document.getElementById("50k").innerHTML = e;
-    document.getElementById("value_alt").innerHTML = f;
-    document.getElementById("parc").innerHTML = "Quanto você precisa?";
-}
+var a = "R$ 10.000";
+        var b = "R$ 20.000";
+        var c = "R$ 30.000";
+        var d = "R$ 40.000";
+        var e = "R$ 50.000";
+        var f = "Outro Valor";
+        document.getElementById("10k").innerHTML = a;
+        document.getElementById("20k").innerHTML = b;
+        document.getElementById("30k").innerHTML = c;
+        document.getElementById("40k").innerHTML = d;
+        document.getElementById("50k").innerHTML = e;
+        document.getElementById("value_alt").innerHTML = f;
+        document.getElementById("parc").innerHTML = "Quanto você precisa?";
+        }
