894d81e08ec2cf2a8ef2105524fe8fd7481ff67c
RzorX <49616719+RzorX@users.noreply.github.com>
2020-04-18 21:50:06 -0300

    update emprestimoform

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,36 +1,17 @@
 /* global google */
 
 let choose = document.getElementById("emprestimo");
-        function calcparcelas() {
-        var a = "6x";
-                var b = "8x";
-                var c = "10x";
-                var d = "12x";
-                var e = "18x";
-                var f = "Voltar";
-                document.getElementById("10k").innerHTML = a;
-                document.getElementById("20k").innerHTML = b;
-                document.getElementById("30k").innerHTML = c;
-                document.getElementById("40k").innerHTML = d;
-                document.getElementById("50k").innerHTML = e;
-                document.getElementById("value_alt").innerHTML = f;
-                document.getElementById("parc").innerHTML = "Em quantas vezes?";
-                }
+function calcparcelas(value) {
+    document.getElementById("valempresitmo").hidden = true;
+    document.getElementById("parcempresitmo").hidden = false;
+    document.getElementById("parc").innerHTML = "Em quantas vezes?";
+    document.getElementById("escolhidos").innerHTML = "Você escolheu: " + value;
+}
 
-function digitarvalor() {
-var a = "R$ 10.000";
-        var b = "R$ 20.000";
-        var c = "R$ 30.000";
-        var d = "R$ 40.000";
-        var e = "R$ 50.000";
-        var f = "Outro Valor";
-        document.getElementById("10k").innerHTML = a;
-        document.getElementById("20k").innerHTML = b;
-        document.getElementById("30k").innerHTML = c;
-        document.getElementById("40k").innerHTML = d;
-        document.getElementById("50k").innerHTML = e;
-        document.getElementById("value_alt").innerHTML = f;
-        document.getElementById("parc").innerHTML = "Quanto você precisa?";
-        }
-        
-        
+function voltar() {
+    document.getElementById("parcempresitmo").hidden = true;
+    document.getElementById("valempresitmo").hidden = false;
+    document.getElementById("parc").innerHTML = "Quanto você precisa?";
+    document.getElementById("escolhidos").innerHTML = null;
+}
+  
