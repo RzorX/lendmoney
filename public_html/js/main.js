7d89e8bf07cd99037ff299c9eecfdc77bd2bc732
RzorX <49616719+RzorX@users.noreply.github.com>
2020-04-19 18:13:03 -0300

    finalizando front

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -5,7 +5,7 @@ function calcparcelas(value) {
     document.getElementById("valempresitmo").hidden = true;
     document.getElementById("parcempresitmo").hidden = false;
     document.getElementById("parc").innerHTML = "Em quantas vezes?";
-    document.getElementById("escolhidos").innerHTML = "Você escolheu: " + value;
+    document.getElementById("escolhidos").innerHTML = "Você selecionou " + value;
 }
 
 function voltar() {
@@ -14,4 +14,11 @@ function voltar() {
     document.getElementById("parc").innerHTML = "Quanto você precisa?";
     document.getElementById("escolhidos").innerHTML = null;
 }
-  
+
+function liberatermo() {
+    if(document.getElementById("exampleCheck1").checked === true){
+    document.getElementById("aceitocondições").disabled = false;
+    } else {
+    document.getElementById("aceitocondições").disabled = true;        
+    }
+}
